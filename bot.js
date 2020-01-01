0056ed053e3a402111f6039db7a8d0853433ae32
Mrahmed380 <58033443+Mrahmed380@users.noreply.github.com>
2020-01-01 22:30:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('message', message => {
   if (!message.content.startsWith(prefix)) return;
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
-  if (message.author.id !== "569892612178444299") return;
+  if (message.author.id !== "639197414393249804") return;
 
   
   if (message.content.startsWith(prefix + 'setwatch')) {
